d0503f789ffae29dce698681b072b51055e59ad8
root <root@li1926-83.members.linode.com>
2018-12-11 20:04:42 +0000

    third commit

--- myproject.py
+++ myproject.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 application = Flask(__name__)
 
+@application.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
 @application.route("/")
 def hello():
     return render_template('index.html') 
